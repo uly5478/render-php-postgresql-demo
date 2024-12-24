412af0404c7c866828569d4d3ad9106111bfbc2e
uly chen <uly.chen@gmail.com>
2024-12-24 10:02:27 +0800

    add recipesController

---
+++ app/Http/Controllers/RecipesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipesController extends Controller
+{
+    public function first()
+    {
+        return 'Hi 我們的第一個Controller';
+    }
+}
